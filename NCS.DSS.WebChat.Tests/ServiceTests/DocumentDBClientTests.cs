94cedfe395054843a6e4ec6394288aa9da145aaf
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-10-12 12:45:18 +0100

    Add Unit Tests for Helpers and Services

---
+++ NCS.DSS.WebChat.Tests/ServiceTests/DocumentDBClientTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Documents.Client;
+using NCS.DSS.WebChat.Cosmos.Client;
+using NUnit.Framework;
+
+namespace NCS.DSS.WebChat.Tests.ServiceTests
+{
+    [TestFixture]
+    public class DocumentDBClientTests
+    {
+        [Test]
+        public void DocumentDBClientTests_ReturnsStatusCodeOK_WhenHttpResponseMessageOkIsCalledWithGuid()
+        {
+            var client = DocumentDBClient.CreateDocumentClient();
+
+            Assert.IsInstanceOf<DocumentClient>(client);
+            Assert.IsNotNull(client);
+        }
+    }
+}
---
+++ NCS.DSS.WebChat.Tests/ServiceTests/DocumentDBHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NCS.DSS.WebChat.Cosmos.Helper;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace NCS.DSS.WebChat.Tests.ServiceTests
+{
+    [TestFixture]
+    public class DocumentDBHelperTests
+    {
+        [Test]
+        public void DocumentDBHelperTests_ReturnsURI_WhenCreateDocumentUriIsCalled()
+        {
+            var uri = DocumentDBHelper.CreateDocumentUri(Arg.Any<Guid>());
+
+            Assert.IsInstanceOf<Uri>(uri);
+            Assert.IsNotNull(uri);
+        }
+        
+        [Test]
+        public void DocumentDBHelperTests_ReturnsURI_WhenDocumentCollectionUriIsCalled()
+        {
+            var uri = DocumentDBHelper.CreateDocumentCollectionUri();
+
+            Assert.IsInstanceOf<Uri>(uri);
+            Assert.IsNotNull(uri);
+        }
+
+        [Test]
+        public void DocumentDBHelperTests_ReturnsURI_WhenCustomerCreateDocumentUriIsCalled()
+        {
+            var uri = DocumentDBHelper.CreateCustomerDocumentUri(Arg.Any<Guid>());
+
+            Assert.IsInstanceOf<Uri>(uri);
+            Assert.IsNotNull(uri);
+        }
+
+        [Test]
+        public void DocumentDBHelperTests_ReturnsURI_WhenCustomerDocumentCollectionUriIsCalled()
+        {
+            var uri = DocumentDBHelper.CreateCustomerDocumentCollectionUri();
+
+            Assert.IsInstanceOf<Uri>(uri);
+            Assert.IsNotNull(uri);
+        }
+    }
+}
