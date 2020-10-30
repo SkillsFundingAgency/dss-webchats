88050be4038dd71c20877139da672e751b3273da
Huss <57395585+anefiox@users.noreply.github.com>
2020-10-30 12:32:33 +0000

    Test upgrade

--- NCS.DSS.WebChat.Tests/GetWebChatByIdHttpTriggerTests.cs
+++ NCS.DSS.WebChat.Tests/GetWebChatByIdHttpTriggerTests.cs
@@ -6,55 +6,64 @@ using Microsoft.Extensions.Logging;
 using NCS.DSS.WebChat.Cosmos.Helper;
 using NCS.DSS.WebChat.GetWebChatByIdHttpTrigger.Service;
 using NCS.DSS.WebChat.Helpers;
-using NSubstitute;
+using Moq;
 using NUnit.Framework;
+using Microsoft.AspNetCore.Http.Internal;
+using DFC.HTTP.Standard;
 
 namespace NCS.DSS.WebChat.Tests
 {
     [TestFixture]
     public class GetWebChatByIdHttpTriggerTests
     {
+        //private readonly Guid _interactionId = Guid.Parse("aa57e39e-4469-4c79-a9e9-9cb4ef410382");
         private const string ValidCustomerId = "7E467BDB-213F-407A-B86A-1954053D3C24";
         private const string ValidInteractionId = "1e1a555c-9633-4e12-ab28-09ed60d51cb3";
         private const string ValidWebChatId = "d5369b9a-6959-4bd3-92fc-1583e72b7e51";
         private const string InValidId = "1111111-2222-3333-4444-555555555555";
 
-        private ILogger _log;
-        private HttpRequestMessage _request;
-        private IResourceHelper _resourceHelper;
-        private IGetWebChatByIdHttpTriggerService _getWebChatByIdHttpTriggerService;
-        private IHttpRequestMessageHelper _httpRequestMessageHelper;
+        //private HttpRequestMessage _request;
+        private Mock<ILogger> _log;
+        private DefaultHttpRequest _request;
+        private Mock<IResourceHelper> _resourceHelper;
+        private Mock<IHttpRequestHelper> _httpRequestMessageHelper;
+        private Mock<IGetWebChatByIdHttpTriggerService> _getWebChatByIdHttpTriggerService;
         private Models.WebChat _webChat;
+        private GetWebChatByIdHttpTrigger.Function.GetWebChatByIdHttpTrigger function;
 
         [SetUp]
         public void Setup()
         {
-            _webChat = Substitute.For<Models.WebChat>();
+            _webChat = new Models.WebChat();
 
-            _request = new HttpRequestMessage()
-            {
-                Content = new StringContent(string.Empty),
-                RequestUri = 
-                    new Uri($"http://localhost:7071/api/Customers/7E467BDB-213F-407A-B86A-1954053D3C24/" +
-                            $"Customers/7E467BDB-213F-407A-B86A-1954053D3C24/" +
-                            $"Interactions/1e1a555c-9633-4e12-ab28-09ed60d51cb3/" +
-                            $"WebChats/d5369b9a-6959-4bd3-92fc-1583e72b7e51")
-            };
+            _request = null;
+            _log = new Mock<ILogger>();
+            _resourceHelper = new Mock<IResourceHelper>();
+            _getWebChatByIdHttpTriggerService = new Mock<IGetWebChatByIdHttpTriggerService>();
+            _httpRequestMessageHelper = new Mock<IHttpRequestHelper>();
 
-            _log = Substitute.For<ILogger>();
-            _resourceHelper = Substitute.For<IResourceHelper>();
-            _getWebChatByIdHttpTriggerService = Substitute.For<IGetWebChatByIdHttpTriggerService>();
-            _httpRequestMessageHelper = Substitute.For<IHttpRequestMessageHelper>();
-            _httpRequestMessageHelper.GetTouchpointId(_request).Returns("0000000001");
+            function = new GetWebChatByIdHttpTrigger.Function.GetWebChatByIdHttpTrigger(_resourceHelper.Object,
+                _httpRequestMessageHelper.Object, _getWebChatByIdHttpTriggerService.Object);
         }
 
         [Test]
         public async Task GetWebChatByIdHttpTrigger_ReturnsStatusCodeBadRequest_WhenTouchpointIdIsNotProvided()
         {
-            _httpRequestMessageHelper.GetTouchpointId(_request).Returns((string)null);
+            // Arrange
+            _httpRequestMessageHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns<string>(null);
 
             // Act
             var result = await RunFunction(InValidId, ValidInteractionId, ValidWebChatId);
+
+            // Assert
+            Assert.IsInstanceOf<HttpResponseMessage>(result);
+            Assert.AreEqual(HttpStatusCode.BadRequest, result.StatusCode);
+
+            // Arrange Interaction
+            _httpRequestMessageHelper.Setup(x => x.GetDssTouchpointId(_request)).Returns<string>(null);
+
+            // Act
+            var result = await RunFunction(ValidCustomerId, ValidInteractionId);
 
             // Assert
             Assert.IsInstanceOf<HttpResponseMessage>(result);
