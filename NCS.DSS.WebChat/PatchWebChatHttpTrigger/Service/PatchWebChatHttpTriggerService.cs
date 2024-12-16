d4044935f69987290952461d170daaa0cec8cb7a
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-12-16 12:14:06 +0000

    additional logging for PATCH service

--- NCS.DSS.WebChat/PatchWebChatHttpTrigger/Service/PatchWebChatHttpTriggerService.cs
+++ NCS.DSS.WebChat/PatchWebChatHttpTrigger/Service/PatchWebChatHttpTriggerService.cs
@@ -1,4 +1,5 @@
-﻿using NCS.DSS.WebChat.Cosmos.Provider;
+﻿using Microsoft.Extensions.Logging;
+using NCS.DSS.WebChat.Cosmos.Provider;
 using NCS.DSS.WebChat.Models;
 using NCS.DSS.WebChat.ServiceBus;
 using System.Net;
@@ -9,33 +10,44 @@ namespace NCS.DSS.WebChat.PatchWebChatHttpTrigger.Service
     {
         private readonly IWebChatServiceBusClient _serviceBusClient;
         private readonly ICosmosDBProvider _cosmosDbProvider;
+        private readonly ILogger<PatchWebChatHttpTriggerService> _logger;
 
-        public PatchWebChatHttpTriggerService(ICosmosDBProvider cosmosDbProvider, IWebChatServiceBusClient serviceBusClient)
+        public PatchWebChatHttpTriggerService(ICosmosDBProvider cosmosDbProvider, 
+            IWebChatServiceBusClient serviceBusClient,
+            ILogger<PatchWebChatHttpTriggerService> logger)
         {
             _cosmosDbProvider = cosmosDbProvider;
             _serviceBusClient = serviceBusClient;
+            _logger = logger;
         }
         public async Task<Models.WebChat> UpdateAsync(Models.WebChat webChat, WebChatPatch webChatPatch)
         {
             if (webChat == null)
+            {
+                _logger.LogWarning("WebChat Object not found");
                 return null;
+            }
 
+            _logger.LogInformation("Preparing PATCH request for WebChat with ID {WebChatId}", webChat.WebChatId);
             webChat.Patch(webChatPatch);
+            _logger.LogInformation("Attempting to Set Default values for WebChat with ID {WebChatId}", webChat.WebChatId);
             webChat.SetDefaultValues();
 
+            _logger.LogInformation("Attempting to Update WebChat Record for {WebChatId}", webChat.WebChatId);
             var response = await _cosmosDbProvider.UpdateWebChatAsync(webChat);
 
-            var responseStatusCode = response.StatusCode;
-
-            return responseStatusCode == HttpStatusCode.OK ? webChat : null;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                _logger.LogInformation("Successfully Updated WebChat Record for {WebChatId}", webChat.WebChatId);
+                return response.Resource;
+            }
+            _logger.LogInformation("Failed to Updated WebChat Record for {WebChatId} and Response Code {StatusCode}", webChat.WebChatId, response.StatusCode);
+            return null;                       
         }
 
         public async Task<Models.WebChat> GetWebChatForCustomerAsync(Guid customerId, Guid interactionId, Guid webChatId)
         {
-
-            var webChat = await _cosmosDbProvider.GetWebChatForCustomerAsync(customerId, interactionId, webChatId);
-
-            return webChat;
+            return await _cosmosDbProvider.GetWebChatForCustomerAsync(customerId, interactionId, webChatId); ;
         }
 
         public async Task SendToServiceBusQueueAsync(Models.WebChat webChat, Guid customerId, string reqUrl)
