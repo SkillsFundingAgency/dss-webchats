6afcc745b27ffedd45e9631d413f15a97117912f
Pradeep Kumar Voorukonda <PradeepKumar.VOORUKONDA@EDUCATION.GOV.UK>
2024-12-16 12:39:06 +0000

    RunAsyn not working for PATCH and POST Requests

--- NCS.DSS.WebChat.Tests/PostWebChatHttpTriggerTests.cs
+++ NCS.DSS.WebChat.Tests/PostWebChatHttpTriggerTests.cs
@@ -202,7 +202,7 @@ namespace NCS.DSS.WebChat.Tests
 
         private async Task<IActionResult> RunFunction(string customerId, string interactionId)
         {
-            return await function.RunAsync(
+            return await function.Run(
                 _request, customerId, interactionId).ConfigureAwait(false);
         }
 
